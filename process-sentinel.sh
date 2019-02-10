859ce50646a8893ed8c2c7df17547209dc48b397
L. David Baron <dbaron@dbaron.org>
2019-02-09 22:01:52 -0800

    Allow merging of multiple files.

--- process-sentinel.sh
+++ process-sentinel.sh
@@ -37,52 +37,72 @@
 # to 5GiB.  (This was needed on Ubuntu but not on Mac OS X.)
 
 # This script creates a directory for temporary storage as a
-# subdirectory of the directory containing its .zip file argument.
+# subdirectory of the directory containing its .zip file argument(s).
 
 # It may require adjusting the CONTRAST and MIDPOINT variables below.
 
 # The output will be a tif file next to the input .zip.
 
-if [ "$#" != "1" ]
+if [ $# -lt 1 ]
 then
-    echo "Expected single argument (.zip file)" 1>&2
+    echo "Expected argument(s) (.zip file(s))" 1>&2
     exit 1
 fi
 
-DIR="$(dirname "$1")"
-ZIPFILE="$(basename "$1")"
+for ((i = 1; $i <= $#; i=$i + 1))
+do
+    ZIPFILE="${!i}"
+    if ! echo "$ZIPFILE" | grep -q ".zip$"
+    then
+        echo "Expected argument .zip file" 1>&2
+        exit 1
+    fi
 
-if ! echo "$ZIPFILE" | grep -q ".zip$"
-then
-    echo "Expected argument .zip file" 1>&2
-    exit 1
-fi
-
-if [ ! -f "$ZIPFILE" ]
-then
-    echo "Expected argument .zip file to exist" 1>&2
-    exit 1
-fi
-
-FULL="$(echo -n "$ZIPFILE" | sed 's/.zip$//')"
+    if [ ! -f "$ZIPFILE" ]
+    then
+        echo "Expected argument .zip file to exist" 1>&2
+        exit 1
+    fi
+done
 
 TMPDIR="$(mktemp -d "$(dirname "$1")"/tmpdir.XXXXXXXX)"
+
+FULL=""
+B2FILES=""
+B3FILES=""
+B4FILES=""
+
+for ((i = 1; $i <= $#; i=$i + 1))
+do
+    ZIPFILE="${!i}"
+    FULL="$FULL$(basename "$ZIPFILE" | sed 's/.zip$//')-"
+
+    B2PATH="$(unzip -qql "$ZIPFILE" | grep "_B02.jp2" | head -1 | cut -b31-)"
+    B3PATH="$(echo $B2PATH | sed 's/_B02.jp2/_B03.jp2/')"
+    B4PATH="$(echo $B2PATH | sed 's/_B02.jp2/_B04.jp2/')"
+    unzip -j "$ZIPFILE" "$B2PATH" "$B3PATH" "$B4PATH" -d "$TMPDIR" || exit $?
+    B2JP2="$(basename "$B2PATH")"
+    B3JP2="$(basename "$B3PATH")"
+    B4JP2="$(basename "$B4PATH")"
+    gdal_translate "$TMPDIR/$B2JP2" "$TMPDIR/B02-$i.tif" || exit $?
+    B2FILES="$B2FILES B02-$i.tif"
+    "rm" "$TMPDIR/$B2JP2"
+    gdal_translate "$TMPDIR/$B3JP2" "$TMPDIR/B03-$i.tif" || exit $?
+    B3FILES="$B3FILES B03-$i.tif"
+    "rm" "$TMPDIR/$B3JP2"
+    gdal_translate "$TMPDIR/$B4JP2" "$TMPDIR/B04-$i.tif" || exit $?
+    B4FILES="$B4FILES B04-$i.tif"
+    "rm" "$TMPDIR/$B4JP2"
+done
+
 cd "$TMPDIR" || exit $?
-B2PATH="$(unzip -qql "../$ZIPFILE" | grep "_B02.jp2" | head -1 | cut -b31-)"
-B3PATH="$(echo $B2PATH | sed 's/_B02.jp2/_B03.jp2/')"
-B4PATH="$(echo $B2PATH | sed 's/_B02.jp2/_B04.jp2/')"
-unzip -j "../$ZIPFILE" "$B2PATH" "$B3PATH" "$B4PATH" || exit $?
-B2JP2="$(basename "$B2PATH")"
-B3JP2="$(basename "$B3PATH")"
-B4JP2="$(basename "$B4PATH")"
-gdal_translate "$B2JP2" B02.tif || exit $?
-gdal_translate "$B3JP2" B03.tif || exit $?
-gdal_translate "$B4JP2" B04.tif || exit $?
-"rm" "$B2JP2" "$B3JP2" "$B4JP2"
-gdalwarp -t_srs EPSG:3857 B02.tif B02-projected.tif || exit $?
-gdalwarp -t_srs EPSG:3857 B03.tif B03-projected.tif || exit $?
-gdalwarp -t_srs EPSG:3857 B04.tif B04-projected.tif || exit $?
-"rm" B02.tif B03.tif B04.tif
+
+gdalwarp -t_srs EPSG:3857 $B2FILES B02-projected.tif || exit $?
+"rm" $B2FILES
+gdalwarp -t_srs EPSG:3857 $B3FILES B03-projected.tif || exit $?
+"rm" $B3FILES
+gdalwarp -t_srs EPSG:3857 $B4FILES B04-projected.tif || exit $?
+"rm" $B4FILES
 
 listgeo -tfw B02-projected.tif || exit $?
 mv B02-projected.tfw RGB.tfw || exit $?
@@ -103,7 +123,7 @@ convert B0{4,3,2}-8bit.tif -combine RGB.tif || exit $?
 "rm" B02-8bit.tif B03-8bit.tif B04-8bit.tif
 gdal_edit.py -a_srs EPSG:3857 RGB.tif || exit $?
 "rm" RGB.tfw
-mv RGB.tif "../${FULL}-RGB-${CONTRAST}x${MIDPOINT}.tif"
+mv RGB.tif "../${FULL}RGB-${CONTRAST}x${MIDPOINT}.tif"
 
 cd .. || exit $?
 rmdir "$TMPDIR"
